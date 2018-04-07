cf56d9efeec33445f941821bde352ff30e324e09
minhnguyenwp <minhnguyenwp@gmail.com>
2018-04-07 16:03:40 +0700

    M - create source

---
+++ app/global-styles.js
@@ -0,0 +1,32 @@
+import { injectGlobal } from 'styled-components';
+// import 'bootstrap/scss/bootstrap.scss';
+// import 'styles/app.scss';
+
+/* eslint no-unused-expressions: 0 */
+injectGlobal`
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  body.fontLoaded {
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  #app {
+    background-color: #fafafa;
+    min-height: 100%;
+    min-width: 100%;
+  }
+
+  p,
+  label {
+    font-family: Georgia, Times, 'Times New Roman', serif;
+    line-height: 1.5em;
+  }
+`;
