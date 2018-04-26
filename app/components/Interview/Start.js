7a05a6f58bd39e4c1e42ebf585cf5538f543647f
minhnguyenwp <minhnguyenwp@gmail.com>
2018-04-26 19:13:34 +0700

    M - change to 1 page logic

---
+++ app/components/Interview/Start.js
@@ -0,0 +1,50 @@
+/*
+ * Login Page
+ */
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Img from 'components/Img';
+import { myFormatDate } from 'utils/helper';
+
+export default class InterviewStart extends React.Component { 
+  startInterview(e){
+    e.preventDefault();
+    if(typeof this.props.startInterview == 'function'){
+        console.log('startInterview');
+        this.props.startInterview(e);
+    }
+  }
+  render() {
+    const {session, sessionDesc} = this.props
+    return (
+        <div className="central-wrap">
+            <Helmet>
+          <title>Dashboard</title>
+          <meta name="description" content="Central Test" />
+        </Helmet>
+        <div className="container">
+          {session && 
+          <div className="content-wrapper">
+            <h2 className="page-ttl uppercase">{session.title}</h2>
+            <div className="page-desc">
+              <p>
+              {sessionDesc.replace('%%readingTimeLimit%%', session.readingTimeLimit).replace('%%answerTimeLimit%%', Math.round(session.answerTimeLimit/60))}
+              </p>
+              <p>
+              Your could try a Practice Interview before taking an actual interview.
+              </p>
+            </div>
+            <div className="btn-wrap">
+              <div className="time-submiss">
+                {'Submission Deadline: ' + myFormatDate('dd-mm-yyyy', session.deadline)} 
+              </div>
+              <a href="/question" className="btn btn-blue">Try a Practice Interview</a>
+              <a onClick={(e) => this.startInterview(e)} className="btn btn-red">Begin Interview</a>
+            </div>
+          </div>
+          }
+        </div>
+        </div>
+    );
+  }
+}
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -19,11 +19,20 @@ import reducer from './reducer';
 import saga from './saga';
 import { getSession } from './actions';
 import { makeSelectSession, makeSelectSessionLoading, makeSelectSessionError } from './selectors';
+import InterviewQuestion from 'components/Interview/Question';
+import InterviewPrepare from 'components/Interview/Prepare';
+import InterviewRecording from 'components/Interview/Recording';
+import InterviewStart from 'components/Interview/Start';
 
 export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      qNum : 0,
+      qStep : 'Start',
+      anwser: false
+    }
+  }
 
   componentDidMount() {
     this.props.onPageLoad();
@@ -35,41 +44,22 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
 
   startInterview(e) {
     e.preventDefault();
-    localStorage.setItem('interviewAnwsers', JSON.stringify(this.props.session.answers));
-    this.props.history.push('/question/1');
+    this.setState({
+      qNum : this.state.qNum + 1,
+      qStep : 'Question'
+    })
   }
 
   render() {
     const {loading, error, session} = this.props
+    const { qNum } = this.state
+    console.log(this.state)
     const sessionDesc = 'In an video interview, you would be given %%readingTimeLimit%% secs to read each question. When the countdown timer reaches "0", the system will automatically begin recording your response. And you\'ll be given a maximum of %%answerTimeLimit%% minutes per question to complete your response. Please note that there are no re-takes for Video Interviews and it will be  a one-time through recording.';
     return (
-      <article className="central-wrap">
-        <Helmet>
-          <title>Dashboard</title>
-          <meta name="description" content="Central Test" />
-        </Helmet>
-        <div className="container">
-          {session && 
-          <div className="content-wrapper">
-            <h2 className="page-ttl uppercase">{session.title}</h2>
-            <div className="page-desc">
-              <p>
-              {sessionDesc.replace('%%readingTimeLimit%%', session.readingTimeLimit).replace('%%answerTimeLimit%%', Math.round(session.answerTimeLimit/60))}
-              </p>
-              <p>
-              Your could try a Practice Interview before taking an actual interview.
-              </p>
-            </div>
-            <div className="btn-wrap">
-              <div className="time-submiss">
-                {'Submission Deadline: ' + myFormatDate('dd-mm-yyyy', session.deadline)} 
-              </div>
-              <a href="/question" className="btn btn-blue">Try a Practice Interview</a>
-              <a onClick={(e) => this.startInterview(e)} className="btn btn-red">Begin Interview</a>
-            </div>
-          </div>
-          }
-        </div>
+      <article>
+      {
+        session && <InterviewStart sessionDesc={sessionDesc} session={session} startInterview={(e) => this.startInterview(e)} />
+      }
       </article>
     );
   }
@@ -86,6 +76,7 @@ HomePage.propTypes = {
     PropTypes.bool,
   ]),
   onPageLoad: PropTypes.func,
+  startInterview: PropTypes.func,
 };
 
 export function mapDispatchToProps(dispatch) {
